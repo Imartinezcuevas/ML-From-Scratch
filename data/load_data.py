6b48801027758270d80d85c58441568e26e0be6b
Iván Martínez Cuevas <imartinezcuevas@gmail.com>
2025-09-18 14:11:03 +0200

    Refactor data loading and update linear regression notebook

--- data/load_data.py
+++ data/load_data.py
@@ -3,9 +3,7 @@ TODO: Functions to load datasets from scikit-learn for Linear Regression exercis
 """
 
 from sklearn.datasets import make_regression
-import numpy as np
 
-def load_synthetic_regression(n_samples=100, n_features=1, noise=0.1, random_state=None):
-    bias = np.random.uniform(low=-10, high=10)
+def load_synthetic_regression(n_samples=100, n_features=1, noise=0.1, random_state=None, bias=None):
     X, y = make_regression(n_samples=n_samples, n_features=n_features, noise=noise, random_state=random_state, bias=bias)
     return X, y, bias
